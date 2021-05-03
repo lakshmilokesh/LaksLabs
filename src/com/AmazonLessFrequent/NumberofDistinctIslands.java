e4a4f2b8b5efa000df5b7ec6ebd2dfaba84ee368
Lakshmi Lokesh <lakshmi.rlokesh@gmail.com>
2021-05-03 16:27:14 -0700

    Update NumberofDistinctIslands.java

--- src/com/AmazonLessFrequent/NumberofDistinctIslands.java
+++ src/com/AmazonLessFrequent/NumberofDistinctIslands.java
@@ -28,39 +28,37 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class NumberofDistinctIslands {
-
-    class Solution {
-        int[][] grid;
-        boolean[][] seen;
-        Set<Integer> shape;
-
-        public void explore(int r, int c, int r0, int c0) {
-            if (0 <= r && r < grid.length && 0 <= c && c < grid[0].length &&
-                    grid[r][c] == 1 && !seen[r][c]) {
-                seen[r][c] = true;
-                shape.add((r - r0) * 2 * grid[0].length + (c - c0));
-                explore(r+1, c, r0, c0);
-                explore(r-1, c, r0, c0);
-                explore(r, c+1, r0, c0);
-                explore(r, c-1, r0, c0);
+    
+    private int[][] grid;
+    private boolean[][] seen;
+    private Set<Pair<Integer, Integer>> currentIsland;
+    private int currRowOrigin;
+    private int currColOrigin;
+    
+    private void dfs(int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return;
+        if (grid[row][col] == 0 || seen[row][col]) return;
+        seen[row][col] = true;
+        currentIsland.add(new Pair<>(row - currRowOrigin, col - currColOrigin));
+        dfs(row + 1, col);
+        dfs(row - 1, col);
+        dfs(row, col + 1);
+        dfs(row, col - 1);    
+    }
+    
+    public int numDistinctIslands(int[][] grid) {
+        this.grid = grid;
+        this.seen = new boolean[grid.length][grid[0].length];   
+        Set<Set<Pair<Integer, Integer>>> islands = new HashSet<>();
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                this.currentIsland = new HashSet<>();
+                this.currRowOrigin = row;
+                this.currColOrigin = col;
+                dfs(row, col);
+                if (!currentIsland.isEmpty()) islands.add(currentIsland);
             }
-        }
-        public int numDistinctIslands(int[][] grid) {
-            this.grid = grid;
-            seen = new boolean[grid.length][grid[0].length];
-            Set shapes = new HashSet<HashSet<Integer>>();
-
-            for (int r = 0; r < grid.length; r++) {
-                for (int c = 0; c < grid[0].length; c++) {
-                    shape = new HashSet<Integer>();
-                    explore(r, c, r, c);
-                    if (!shape.isEmpty()) {
-                        shapes.add(shape);
-                    }
-                }
-            }
-
-            return shapes.size();
-        }
+        }         
+        return islands.size();
     }
 }
