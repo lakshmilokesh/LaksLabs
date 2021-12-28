bf60fbc2a3e6c7224386fafd7621969aed1080c7
Lakshmi Lokesh <lakshmi.rlokesh@gmail.com>
2021-12-28 15:30:23 -0800

    Update BinaryTreeMaximumPathSum.java

--- src/com/Facebook2020/BinaryTreeMaximumPathSum.java
+++ src/com/Facebook2020/BinaryTreeMaximumPathSum.java
@@ -1,4 +1,20 @@
 package com.Facebook2020;
 
 public class BinaryTreeMaximumPathSum {
+  
+    int res = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        solve(root);
+        return res;
+    }
+    public int solve(TreeNode root){
+        if(root == null)return 0;
+        int left = solve(root.left);
+        int right = solve(root.right);
+        
+        int temp = Math.max(Math.max(left,right) + root.val,root.val);
+        int ans = Math.max(temp,left+right+root.val);
+        res = Math.max(ans,res);
+        return temp;
+    }
 }
