c6c273e99356a1f30f254f6cc582f3e344b5dee2
Lakshmi Lokesh <lakshmi.rlokesh@gmail.com>
2021-12-31 13:53:07 -0800

    2021 updates

--- src/com/AmazonLessFrequent/ClosestLeafBinaryTree.java
+++ src/com/AmazonLessFrequent/ClosestLeafBinaryTree.java
@@ -26,20 +26,78 @@ Explanation: Either 2 or 3 is the nearest leaf node to the target of 1.
  */
 
 import com.binarytree.TreeNode;
+ /**
+ 2  * Definition for a binary tree node.
+ 3  * public class TreeNode {
+ 4  *     int val;
+ 5  *     TreeNode left;
+ 6  *     TreeNode right;
+ 7  *     TreeNode() {}
+ 8  *     TreeNode(int val) { this.val = val; }
+ 9  *     TreeNode(int val, TreeNode left, TreeNode right) {
+10  *         this.val = val;
+11  *         this.left = left;
+12  *         this.right = right;
+13  *     }
+14  * }
+15  */
+16 class Solution {
+17     TreeNode target;
+18 
+19     public int findClosestLeaf(TreeNode root, int k) {
+20         HashMap<TreeNode, TreeNode> map = new HashMap<>();
+21         dfs(root, null, map, k);
+22         if (target == null) {
+23             return -1;
+24         }
+25 
+26         Queue<TreeNode> queue = new LinkedList<>();
+27         HashSet<Integer> visited = new HashSet<>();
+28         queue.offer(target);
+29         visited.add(k);
+30         while (!queue.isEmpty()) {
+31             TreeNode cur = queue.poll();
+32             if (cur.left == null && cur.right == null) {
+33                 return cur.val;
+34             }
+35             if (cur.left != null && !visited.contains(cur.left.val)) {
+36                 queue.offer(cur.left);
+37                 visited.add(cur.left.val);
+38             }
+39             if (cur.right != null && !visited.contains(cur.right.val)) {
+40                 queue.offer(cur.right);
+41                 visited.add(cur.right.val);
+42             }
+43             if (map.containsKey(cur) && !visited.contains(map.get(cur).val)) {
+44                 queue.offer(map.get(cur));
+45                 visited.add(map.get(cur).val);
+46             }
+47         }
+48         return -1;
+49     }
+50 
+51     private void dfs(TreeNode root, TreeNode parent, HashMap<TreeNode, TreeNode> map, int k) {
+52         if (root == null) {
+53             return;
+54         }
+55         if (parent != null) {
+56             map.put(root, parent);
+57         }
+58         if (root.val == k) {
+59             target = root;
+60         }
+61         dfs(root.left, root, map, k);
+62         dfs(root.right, root, map, k);
+63     }
+64 }
 
-public class ClosestLeafBinaryTree {
 
-    // NOT THE RIGHT SOLUTION
-    public int findClosestLeaf(TreeNode root, int target) {
+   
+    
 
-        int ret = 0;
-        while(root != null){
-            ret = root.val;
-            if(Math.abs(target - root.val) < Math.abs(target - ret) && root.left == null && root.right == null){
-                ret = root.val;
-            }
-            root = root.val > target? root.left: root.right;
-        }
-        return ret;
-    }
-}
+     
+    
+        
+         
+            
+     
