b704d2d9f59f79172fb0a43838f83143a4b514b2
Lakshmi Lokesh <lakshmi.rlokesh@gmail.com>
2022-01-01 14:37:51 -0800

    Update FindSingleNumber.java

--- src/com/InterviewQuestions2019/FindSingleNumber.java
+++ src/com/InterviewQuestions2019/FindSingleNumber.java
@@ -9,8 +9,33 @@ package com.InterviewQuestions2019;
  * [0,1,1,2,2,3,3] => 0
  * [0,0,1,1,2,2,3] => 3
  * [0,0,1,1,2,3,3] => 2
+ 
+ The idea is based on the following two facts. 
+a) XOR of a number with itself is 0. 
+b) XOR of a number with 0 is number itself.
  *
  *
  */
 public class FindSingleNumber {
+  
+    // Return the maximum Sum of difference
+    // between consecutive elements.
+    static int findSingle(int ar[], int ar_size)
+    {
+        // Do XOR of all elements and return
+        int res = ar[0];
+        for (int i = 1; i < ar_size; i++)
+            res = res ^ ar[i];
+     
+        return res;
+    }
+ 
+    // Driver code
+    public static void main (String[] args)
+    {
+        int ar[] = {2, 3, 5, 4, 5, 3, 4};
+        int n = ar.length;
+        System.out.println("Element occurring once is " +
+                            findSingle(ar, n) + " ");
+    }
 }
